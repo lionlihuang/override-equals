534b7bf725994e4b84cd8c7321cb3880c29519b7
Administrator <756433556@qq.com>
2020-05-17 20:37:50 +0800

    覆盖equals方法 修正 '&&' 应另起一行。

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -32,8 +32,7 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(name, user.name);
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
     }
 
     @Override
